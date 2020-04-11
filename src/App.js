3edc752c6597c1369491e40447fc8e621e1b43e9
TheBear <44812306+MarcioBBS@users.noreply.github.com>
2020-04-11 10:21:00 -0700

    Create Person component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>React App</h1>         
+        <h1>React App</h1>
+        <span>Ahaaaa now it's working!!!</span>
+        <Person />
       </div>
     );
+
+    // This code below gets compiled to the code above (JSX)
+    //return  React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'I\'m a H1 element'));
+    
   }
 }
 
