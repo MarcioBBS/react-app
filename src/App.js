0f5891131f88c2f4e498e4dfe03ef9407a71e727
TheBear <44812306+MarcioBBS@users.noreply.github.com>
2020-04-14 15:31:09 -0700

    Add two ways to passing methods references between components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-//import React, { Component } from 'react';
+import React, { Component } from 'react';
 
 // useState is the most important REACT HOOK, It allows us to manage state in the function-base component.
-import React, { useState } from 'react'; // For REACT HOOKS
+//import React, { useState } from 'react'; // For REACT HOOKS
 
 import './App.css';
 import Person from './Person/Person';
 
-/*
+
 // Class-base Components: This is the establish/default syntax.
 class App extends Component {
 
@@ -18,10 +18,10 @@ class App extends Component {
     ]
   };
   
-  switchNameHandler = () => {
+  switchNameHandler = (newName) => {
     this.setState ({
       persons: [
-        { name: 'Marcio Braz', age: 41 },
+        { name: newName, age: 41 },
         { name: 'Leah', age: 38 },
         { name: 'Julian', age: 10 }
       ]
@@ -33,10 +33,19 @@ class App extends Component {
       <div className="App">
         <h1>React App</h1>
         <p>Ahaaaa now it's working!!!</p>
-        <button onClick={this.switchNameHandler}>Switch name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>By the way she's my wife!</Person>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+        <button onClick={ () => this.switchNameHandler('Marcio Braz')}>Switch name</button>
+        <Person 
+          name={this.state.persons[0].name} 
+          age={this.state.persons[0].age} />
+        <Person 
+          name={this.state.persons[1].name} 
+          age={this.state.persons[1].age}
+          click = {this.switchNameHandler.bind(this, 'Me!!!')} >
+          By the way she's my wife!
+        </Person>
+        <Person 
+          name={this.state.persons[2].name} 
+          age={this.state.persons[2].age}/>
       </div>
     );
 
@@ -47,15 +56,15 @@ class App extends Component {
 }
 
 export default App;
-*/
 
+/*
 // Functional components - Using REACT HOOKS
 const app = props => {
 
-  /* useState returns a Array with two elements, ALWAYS two elements!!! 
-  The 1st element is the current state
-  The 2nd element is ALWAYS a function that allows us to update the current state and render the component
-  */
+  // useState returns a Array with two elements, ALWAYS two elements!!! 
+  // The 1st element is the current state
+  // The 2nd element is ALWAYS a function that allows us to update the current state and render the component
+  
   const [ personsState, setPersonsState ] = useState({ // the names personsState and setPersonsState are completely optional
     persons: [
       { name: 'Marcio', age: 41 },
@@ -91,3 +100,4 @@ const app = props => {
 };  
 
 export default app;
+*/
