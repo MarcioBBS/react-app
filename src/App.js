97655b97e4308a519dd88824018d5bccc75858ab
TheBear <44812306+MarcioBBS@users.noreply.github.com>
2020-04-13 21:53:30 -0700

    Add setState( ) method

--- src/App.js
+++ src/App.js
@@ -11,13 +11,23 @@ class App extends Component {
       { name: 'Julian', age: 1 }
     ]
   };
+  
+  switchNameHandler = () => {
+    this.setState ({
+      persons: [
+        { name: 'Marcio Braz', age: 41 },
+        { name: 'Leah', age: 38 },
+        { name: 'Julian', age: 10 }
+      ]
+    });
+  }
 
   render() {
     return (
       <div className="App">
         <h1>React App</h1>
         <p>Ahaaaa now it's working!!!</p>
-        <button>Switch name</button>
+        <button onClick={this.switchNameHandler}>Switch name</button>
         <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
         <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>By the way she's my wife!</Person>
         <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
