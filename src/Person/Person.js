1a625bcd2f16fa281a0d2f819c0ec71952ebbf20
TheBear <44812306+MarcioBBS@users.noreply.github.com>
2020-05-27 21:38:02 -0700

    Add CSS Modules & Media Queries

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,33 +1,13 @@
 import React from 'react';
-import './Person.css'
-// import Radium from 'radium';
-import styled from 'styled-components';
-
-// Storing the result of the .div method (from the styled object) inside of StyledDiv constant.
-// Note: Using regular CSS
-// StyledDiv returns a React component, that's the reason the 1st letter is uppercase StyleDiv instead of styledDiv
-const StyledDiv = styled.div `
-
-    width: 60%;
-    margin: 16px auto;
-    border: 1px solid #eee;
-    box-shadow: 0 2px 3px #ccc;
-    padding: 16px;
-    text-align: center;
-
-    @media (min-width: 500px) {
-        width: 450px;
-    }
-`;
+import classes from './Person.css'
 
 const person = props => {
     return (
-        // <div className= "Person" style= {style}>
-        <StyledDiv>
-            <p onClick={props.click}>I'm {props.name} and I'm {props.age}</p>
+        <div className= {classes.Person}>
+            <p onClick= {props.click}>I'm {props.name} and I'm {props.age}</p>
             <p>{props.children}</p>
-            <input type="text" onChange={props.change} placeholder={props.name}/>
-        </StyledDiv>
+            <input type= "text" onChange= {props.change} placeholder= {props.name}/>
+        </div>
         // </div>
     ) 
 }
